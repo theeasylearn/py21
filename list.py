548b48321ada33ae0e0f06d9b15dd16bb796d0af
theeasylearn <theeasylearn@gmail.com>
2024-10-21 09:31:54 +0530

    list

---
+++ list.py
@@ -0,0 +1,10 @@
+#example of list
+family = ['Ram','Sita','Lav','Kush']
+mixed = [True,12,81.25,'Ankit Patel']
+
+print(family)
+print(family[0])
+print(family[1:3])
+print(family[2:])
+print(mixed * 2)
+print(mixed + family)
---
+++ stringdemo.py
@@ -0,0 +1,10 @@
+name = "the easylearn academy"
+print(name)
+print(name[0:3]) # the 
+print(name[4:13]) # easylearn
+print(name[14:]) # academy
+print(name[0]) # t
+name = "T E L";
+print(name)
+name[0] = "THE " #error becasue str is immutable
+print(name)
