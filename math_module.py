d76289a7b819b9a91da1a51fdc3a2a740308508e
devarsh2007 <139302768+devarsh2007@users.noreply.github.com>
2024-11-20 09:20:50 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,11 @@
+import math
+print(dir(math))
+
+print(math.ceil(10.6))
+print(math.ceil(10.2))
+print(math.ceil(-4.3))
+
+print(math.floor(10.5))
+print(math.floor(-4.3))
+
+print(math.factorial(3))
