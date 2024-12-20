e9c04722bb4be0e1ea585cede84c38fde9442770
devarsh2007 <139302768+devarsh2007@users.noreply.github.com>
2024-12-20 08:51:47 +0530

    Add files via upload

---
+++ nestedloop2.py
@@ -0,0 +1,25 @@
+# *  
+# **
+# ***
+# ****
+
+n=int(input("enter number : "))
+# count = 1
+for j in range(1,n+1):
+    for i in range(1,j+1):
+        print("*",end=" ")
+        # print(count)
+    # count+=1
+    print()
+
+# for i in range(2):
+#     print("*",end=" ")
+# print()
+
+# for i in range(3):
+#     print("*",end=" ")
+# print()
+
+# for i in range(4):
+#     print("*",end=" ")
+# print()
